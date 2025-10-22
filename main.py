68a277f6f1d5ffe7615bd97c51ef43e781e103f5
Hamtras <hamtras59@hotmail.com>
2025-10-22 09:20:02 -0500

    initial Commit: Project structure setup

---
+++ main.py
@@ -0,0 +1,12 @@
+# Validación de versión de las librerias
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import requests
+
+print(pd.__version__)
+print(np.__version__)
+print(mpl.__version__)
+print(requests.__version__)
+
